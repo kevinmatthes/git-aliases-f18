40eb674392af3d68b345e6b0d4df513f1c9da2af
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2022-09-03 03:38:04 +0200

    Define service subroutine

---
+++ src/overview.f
@@ -0,0 +1,58 @@
+!!!!!!!!!!!!!!!!!!!!!!!! GNU General Public License 2.0 !!!!!!!!!!!!!!!!!!!!!!!!
+!!                                                                            !!
+!! Copyright (C) 2022 Kevin Matthes                                           !!
+!!                                                                            !!
+!! This program is free software; you can redistribute it and/or modify       !!
+!! it under the terms of the GNU General Public License as published by       !!
+!! the Free Software Foundation; either version 2 of the License, or          !!
+!! (at your option) any later version.                                        !!
+!!                                                                            !!
+!! This program is distributed in the hope that it will be useful,            !!
+!! but WITHOUT ANY WARRANTY; without even the implied warranty of             !!
+!! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              !!
+!! GNU General Public License for more details.                               !!
+!!                                                                            !!
+!! You should have received a copy of the GNU General Public License along    !!
+!! with this program; if not, write to the Free Software Foundation, Inc.,    !!
+!! 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.                !!
+!!                                                                            !!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!
+!> \author      Kevin Matthes
+!> \brief       A brief summary of the command to configure.
+!> \copyright   (C) 2022 Kevin Matthes.
+!>              This file is licensed GPL 2 as of June 1991.
+!> \date        2022
+!> \file        overview.f
+!> \note        See `LICENSE' for full license.
+!>              See `README.md' for project details.
+!>
+!> This source file defines a service subroutine in order write information
+!> regarding the command to configure to `stdout`.
+!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!
+!> \brief   A summary of the command to be configured.
+!> \param   alias   The alias to be configured.
+!> \param   comment A description of the alias to be configured.
+!>
+!> This subroutine will write information about the Git alias to be configured
+!> to `stdout`.
+!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+      subroutine overview (alias, comment)
+      implicit none
+
+      character (*), intent (in)   :: alias
+      character (*), intent (in)   :: comment
+
+      print '(a25, t30, a)', alias, comment
+
+      end
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
