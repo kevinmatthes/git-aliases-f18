f7b50d4b107a730adc32c33c8a327496f581d329
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2022-09-08 20:17:20 +0200

    Create tws.f

---
+++ aliases/tws.f
@@ -0,0 +1,73 @@
+!!!!!!!!!!!!!!!!!!!!!!!! GNU General Public License 2.0 !!!!!!!!!!!!!!!!!!!!!!!!
+!!                                                                            !!
+!! Copyright (C) 2022 Kevin Matthes                                           !!
+!!                                                                            !!
+!! This program is free software; you can redistribute it and/or modify       !!
+!! it under the terms of the GNU General Public License as published by       !!
+!! the Free Software Foundation; either version 2 of the License, or          !!
+!! (at your option) any later version.                                        !!
+!!                                                                            !!
+!! This program is distributed in the hope that it will be useful,            !!
+!! but WITHOUT ANY WARRANTY; without even the implied warranty of             !!
+!! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              !!
+!! GNU General Public License for more details.                               !!
+!!                                                                            !!
+!! You should have received a copy of the GNU General Public License along    !!
+!! with this program; if not, write to the Free Software Foundation, Inc.,    !!
+!! 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.                !!
+!!                                                                            !!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!
+!> \author      Kevin Matthes
+!> \brief       Search the repository for trailing whitespaces.
+!> \copyright   (C) 2022 Kevin Matthes.
+!>              This file is licensed GPL 2 as of June 1991.
+!> \date        2022
+!> \file        withdraw.f
+!> \note        See `LICENSE' for full license.
+!>              See `README.md' for project details.
+!>
+!> This alias is used in order to search all files in the current repository for
+!> trailing whitespaces.  At option, one can also provide an additional path to
+!> search only there.
+!>
+!> Trailing whitespaces do not fulfill any meaningful task in most cases such
+!> that they require storage without any reason.  Furthermore, they are often
+!> considered very annoying during the development of projects.
+!>
+!> This alias command is intended to locate all occurences of those obsolete
+!> tokens in order to be able to remove them easier, if required.
+!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!
+!> \brief   Search the repository for trailing whitespaces.
+!>
+!> This alias is used in order to search all files in the current repository for
+!> trailing whitespaces.  At option, one can also provide an additional path to
+!> search only there.
+!>
+!> Trailing whitespaces do not fulfill any meaningful task in most cases such
+!> that they require storage without any reason.  Furthermore, they are often
+!> considered very annoying during the development of projects.
+!>
+!> This alias command is intended to locate all occurences of those obsolete
+!> tokens in order to be able to remove them easier, if required.
+!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+      subroutine tws
+      implicit none
+
+      call git
+     &( 'tws'
+     &, 'grep [[:blank:]]$'
+     &, 'Search the repository for trailing whitespaces.'
+     &)
+
+      end
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
