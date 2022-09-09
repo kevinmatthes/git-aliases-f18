e8323a26a89a996c93caeca8834aff5690d0699f
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2022-09-09 17:58:20 +0200

    Adjust file name

--- aliases/tws.f
+++ aliases/tws.f
@@ -25,7 +25,7 @@
 !> \copyright   (C) 2022 Kevin Matthes.
 !>              This file is licensed GPL 2 as of June 1991.
 !> \date        2022
-!> \file        withdraw.f
+!> \file        tws.f
 !> \note        See `LICENSE' for full license.
 !>              See `README.md' for project details.
 !>
