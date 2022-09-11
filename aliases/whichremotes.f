54920f4b812f045a9fa2873fc37b2f6492e618bc
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2022-09-11 20:30:25 +0200

    Update whichremotes.f

--- aliases/whichremotes.f
+++ aliases/whichremotes.f
@@ -42,6 +42,7 @@
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
       subroutine whichremotes
+      use libgaf18, only: git
       implicit none
 
       call git
