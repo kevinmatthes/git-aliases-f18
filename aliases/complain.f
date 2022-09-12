a7874b796bdc25fe73fa28b5afedf1e4e89d753c
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2022-09-12 20:13:53 +0200

    Create complain.f

---
+++ aliases/complain.f
@@ -0,0 +1,74 @@
+!!!!!!!!!!!!!!!!!!!!!!!! GNU General Public License 2.0 !!!!!!!!!!!!!!!!!!!!!!!!
+!!                                                                            !!
+!! Copyright (C) 2022 Kevin Matthes                                           !!
+!!                                                                            !!
+!! This program is free software; you can redistribute it and/or modify       !!
+!! it under the terms of the GNU General Public License as published by       !!
+!! the Free Software Foundation; either version 2 of the License, or          !!
+!! (at your option) any later version.                                        !!
+!!                                                                            !!
+!! This program is distributed in the hope that it will be useful,            !!
+!! but WITHOUT ANY WARRANTY; without even the implied warranty of             !!
+!! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              !!
+!! GNU General Public License for more details.                               !!
+!!                                                                            !!
+!! You should have received a copy of the GNU General Public License along    !!
+!! with this program; if not, write to the Free Software Foundation, Inc.,    !!
+!! 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.                !!
+!!                                                                            !!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!
+!> \author      Kevin Matthes
+!> \brief       Search the repository for lines with more than 80 characters.
+!> \copyright   (C) 2022 Kevin Matthes.
+!>              This file is licensed GPL 2 as of June 1991.
+!> \date        2022
+!> \file        complain.f
+!> \note        See `LICENSE' for full license.
+!>              See `README.md' for project details.
+!>
+!> This alias is used in order to search all files in the current repository for
+!> lines with more than eighty characters.  At option, one can also provide an
+!> additional path to search only there.
+!>
+!> A maximum line length is often considered a reasonable design choice.  For
+!> historical reasons, a length of at most eighty characters per line is still
+!> common in many projects.
+!>
+!> This alias command helps finding the lines with more than eighty characters
+!> such that those lines can be adjusted and shortened, if required.
+!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!
+!> \brief   Search the repository for lines with more than 80 characters.
+!>
+!> This alias is used in order to search all files in the current repository for
+!> lines with more than eighty characters.  At option, one can also provide an
+!> additional path to search only there.
+!>
+!> A maximum line length is often considered a reasonable design choice.  For
+!> historical reasons, a length of at most eighty characters per line is still
+!> common in many projects.
+!>
+!> This alias command helps finding the lines with more than eighty characters
+!> such that those lines can be adjusted and shortened, if required.
+!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+      subroutine complain
+      use libgaf18, only: git
+      implicit none
+
+      call git
+     &( 'complain'
+     &, 'grep -E ^.{81,}$'
+     &, 'Search the repository for lines longer than 80.'
+     &)
+
+      end subroutine complain
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
