cb1cd017048fc24e4929a7f9077ecafad51cf83a
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2022-09-13 11:23:35 +0200

    Update whichremotes.f

--- aliases/whichremotes.f
+++ aliases/whichremotes.f
@@ -22,8 +22,7 @@
 !!
 !> \author      Kevin Matthes
 !> \brief       Show the available remote repositories.
-!> \copyright   (C) 2022 Kevin Matthes.
-!>              This file is licensed GPL 2 as of June 1991.
+!> \copyright   GPL-2.0
 !> \date        2022
 !> \file        whichremotes.f
 !> \note        See `LICENSE' for full license.
