737099ff2a5cdb01940bc3fd7f520147db64bc93
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2022-09-13 11:18:12 +0200

    Update goto.f

--- aliases/goto.f
+++ aliases/goto.f
@@ -22,8 +22,7 @@
 !!
 !> \author      Kevin Matthes
 !> \brief       Go to another branch.
-!> \copyright   (C) 2022 Kevin Matthes.
-!>              This file is licensed GPL 2 as of June 1991.
+!> \copyright   GPL-2.0
 !> \date        2022
 !> \file        goto.f
 !> \note        See `LICENSE' for full license.
