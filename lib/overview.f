6dbc2195793a0ec4308a380656268d82f9779bce
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2022-09-13 11:13:59 +0200

    Update overview.f

--- lib/overview.f
+++ lib/overview.f
@@ -22,8 +22,7 @@
 !!
 !> \author      Kevin Matthes
 !> \brief       A brief summary of the command to configure.
-!> \copyright   (C) 2022 Kevin Matthes.
-!>              This file is licensed GPL 2 as of June 1991.
+!> \copyright   GPL-2.0
 !> \date        2022
 !> \file        overview.f
 !> \note        See `LICENSE' for full license.
