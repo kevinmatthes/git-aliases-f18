23c14cf1fc415a9c8331063e950c5300293bf66b
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2022-09-13 11:17:54 +0200

    Update ff.f

--- aliases/ff.f
+++ aliases/ff.f
@@ -22,8 +22,7 @@
 !!
 !> \author      Kevin Matthes
 !> \brief       Merge with the fast-forward strategy.
-!> \copyright   (C) 2022 Kevin Matthes.
-!>              This file is licensed GPL 2 as of June 1991.
+!> \copyright   GPL-2.0
 !> \date        2022
 !> \file        ff.f
 !> \note        See `LICENSE' for full license.
