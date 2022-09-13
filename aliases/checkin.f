649d94e5256f9b41d7025fb3b07c4ce55eda842d
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2022-09-13 15:25:01 +0200

    Expand documentation

--- aliases/checkin.f
+++ aliases/checkin.f
@@ -20,25 +20,36 @@
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!
-!> \author      Kevin Matthes
-!> \brief       Immediately stage and commit all changes at once.
-!> \copyright   GPL-2.0
-!> \date        2022
-!> \file        checkin.f
-!> \note        See `LICENSE' for full license.
-!>              See `README.md' for project details.
-!>
-!> This alias is used in order to stage and commit all changes at once with just
-!> a single Git instruction.
+!> \file checkin.f
 !!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!
+!> \author      Kevin Matthes
+!> \copyright   GPL-2.0
+!> \date        2022
+!> \note        See `LICENSE' for full license.
+!>              See `README.md' for project details.
+!>
 !> \brief   Immediately stage and commit all changes at once.
 !>
 !> This alias is used in order to stage and commit all changes at once with just
-!> a single Git instruction.
+!> a single Git instruction.  In order to avoid option clashes with other passed
+!> options, the command definition was split into multiple Git operations.
+!>
+!> When working on a project, especially on the command line, it is common to
+!> finalise the changes to commit, at first, and to commit them all at once when
+!> the intended progress was achieved.  Then, this alias command comes in handy
+!> in order to persist them with a single instruction.
+!>
+!> Another possible use case is measuring the runtime of applications.  One can
+!> then use this alias in scripts in order to commit the measured data as soon
+!> as it is available.  That way, a loss of data is excluded due to the design
+!> of Git.
+!>
+!> Due to this command's definition, it is possible to pass additional options
+!> to `git commit` as required.
 !!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
