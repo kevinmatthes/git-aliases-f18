9f226472a1577ae04d4b6b70fc5a10be82da7087
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2022-09-13 11:22:58 +0200

    Update view.f

--- aliases/view.f
+++ aliases/view.f
@@ -22,8 +22,7 @@
 !!
 !> \author      Kevin Matthes
 !> \brief       Explore the Git history in the terminal.
-!> \copyright   (C) 2022 Kevin Matthes.
-!>              This file is licensed GPL 2 as of June 1991.
+!> \copyright   GPL-2.0
 !> \date        2022
 !> \file        view.f
 !> \note        See `LICENSE' for full license.
