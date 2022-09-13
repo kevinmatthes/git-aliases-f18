a8f8f366c1a5262adb9f987eb26116ddfa9044f7
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2022-09-13 11:23:53 +0200

    Update withdraw.f

--- aliases/withdraw.f
+++ aliases/withdraw.f
@@ -22,8 +22,7 @@
 !!
 !> \author      Kevin Matthes
 !> \brief       Remove the given files from the staging area.
-!> \copyright   (C) 2022 Kevin Matthes.
-!>              This file is licensed GPL 2 as of June 1991.
+!> \copyright   GPL-2.0
 !> \date        2022
 !> \file        withdraw.f
 !> \note        See `LICENSE' for full license.
