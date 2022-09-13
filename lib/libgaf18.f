c72cbb4182022dec77fe52ee06121aa3aae22da5
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2022-09-13 11:13:37 +0200

    Update libgaf18.f

--- lib/libgaf18.f
+++ lib/libgaf18.f
@@ -22,8 +22,7 @@
 !!
 !> \author      Kevin Matthes
 !> \brief       The business logic of `git-aliases-f18`.
-!> \copyright   (C) 2022 Kevin Matthes.
-!>              This file is licensed GPL 2 as of June 1991.
+!> \copyright   GPL-2.0
 !> \date        2022
 !> \file        libgaf18.f
 !> \note        See `LICENSE' for full license.
