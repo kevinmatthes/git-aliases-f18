17ff710a8a3dba920a463a37771b07a5095adff7
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2022-09-13 11:15:16 +0200

    Update backupdestination.f

--- aliases/backupdestination.f
+++ aliases/backupdestination.f
@@ -22,8 +22,7 @@
 !!
 !> \author      Kevin Matthes
 !> \brief       Configure the default backup remote `backup-drive`.
-!> \copyright   (C) 2022 Kevin Matthes.
-!>              This file is licensed GPL 2 as of June 1991.
+!> \copyright   GPL-2.0
 !> \date        2022
 !> \file        backupdestination.f
 !> \note        See `LICENSE' for full license.
