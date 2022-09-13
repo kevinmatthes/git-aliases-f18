b17c0eb8b54e8df0f630975cb2d4bf0b469d372c
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2022-09-13 11:14:55 +0200

    Update backup.f

--- aliases/backup.f
+++ aliases/backup.f
@@ -22,8 +22,7 @@
 !!
 !> \author      Kevin Matthes
 !> \brief       Backup all commits to the given remote repository.
-!> \copyright   (C) 2022 Kevin Matthes.
-!>              This file is licensed GPL 2 as of June 1991.
+!> \copyright   GPL-2.0
 !> \date        2022
 !> \file        backup.f
 !> \note        See `LICENSE' for full license.
