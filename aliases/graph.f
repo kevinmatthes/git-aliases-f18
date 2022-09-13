cfb877b32859fe297f5ab0ff3b09e7b13da13eae
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2022-09-13 11:18:50 +0200

    Update graph.f

--- aliases/graph.f
+++ aliases/graph.f
@@ -22,8 +22,7 @@
 !!
 !> \author      Kevin Matthes
 !> \brief       Launch the external repository inspection tool.
-!> \copyright   (C) 2022 Kevin Matthes.
-!>              This file is licensed GPL 2 as of June 1991.
+!> \copyright   GPL-2.0
 !> \date        2022
 !> \file        graph.f
 !> \note        See `LICENSE' for full license.
