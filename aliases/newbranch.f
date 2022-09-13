2b3a92aef8ecc2c3a59a11c4f9b9c306c73b7dbe
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2022-09-13 11:19:40 +0200

    Update newbranch.f

--- aliases/newbranch.f
+++ aliases/newbranch.f
@@ -22,8 +22,7 @@
 !!
 !> \author      Kevin Matthes
 !> \brief       Create a branch.
-!> \copyright   (C) 2022 Kevin Matthes.
-!>              This file is licensed GPL 2 as of June 1991.
+!> \copyright   GPL-2.0
 !> \date        2022
 !> \file        newbranch.f
 !> \note        See `LICENSE' for full license.
