1faabb1963f3a333879b2a13f242795c989d134f
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2022-09-22 06:20:45 +0200

    Update main.f

--- src/main.f
+++ src/main.f
@@ -53,32 +53,32 @@
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
       program git_aliases
-      use libgaf18_aliases, only: backup
-      use libgaf18_aliases, only: backupdestination
-      use libgaf18_aliases, only: bone
-      use libgaf18_aliases, only: bugfix
-      use libgaf18_aliases, only: checkin
-      use libgaf18_aliases, only: complain
-      use libgaf18_aliases, only: create
-      use libgaf18_aliases, only: delbranch
-      use libgaf18_aliases, only: ff
-      use libgaf18_aliases, only: goto_git
-      use libgaf18_aliases, only: grab
-      use libgaf18_aliases, only: graph
-      use libgaf18_aliases, only: newbranch
-      use libgaf18_aliases, only: noff
-      use libgaf18_aliases, only: rewind_git
-      use libgaf18_aliases, only: savetags
-      use libgaf18_aliases, only: store
-      use libgaf18_aliases, only: subinit
-      use libgaf18_aliases, only: tidy
-      use libgaf18_aliases, only: tws
-      use libgaf18_aliases, only: unconfigure
-      use libgaf18_aliases, only: verbose
-      use libgaf18_aliases, only: view
-      use libgaf18_aliases, only: whereami
-      use libgaf18_aliases, only: whichremotes
-      use libgaf18_aliases, only: withdraw
+      use libgaf18, only: backup
+      use libgaf18, only: backupdestination
+      use libgaf18, only: bone
+      use libgaf18, only: bugfix
+      use libgaf18, only: checkin
+      use libgaf18, only: complain
+      use libgaf18, only: create
+      use libgaf18, only: delbranch
+      use libgaf18, only: ff
+      use libgaf18, only: goto_git
+      use libgaf18, only: grab
+      use libgaf18, only: graph
+      use libgaf18, only: newbranch
+      use libgaf18, only: noff
+      use libgaf18, only: rewind_git
+      use libgaf18, only: savetags
+      use libgaf18, only: store
+      use libgaf18, only: subinit
+      use libgaf18, only: tidy
+      use libgaf18, only: tws
+      use libgaf18, only: unconfigure
+      use libgaf18, only: verbose
+      use libgaf18, only: view
+      use libgaf18, only: whereami
+      use libgaf18, only: whichremotes
+      use libgaf18, only: withdraw
       implicit none
 
       print '(a / a / a / a / a //// a20, t24, a /)'
