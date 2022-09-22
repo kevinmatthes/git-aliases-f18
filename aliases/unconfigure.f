2aab5c77352f8b0faaa51dc468e634b1b1faf9e4
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2022-09-22 05:22:39 +0200

    Expand documentation

--- aliases/unconfigure.f
+++ aliases/unconfigure.f
@@ -20,21 +20,18 @@
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!
-!> \author      Kevin Matthes
-!> \brief       Remove a configuration entry from this user's Git settings.
-!> \copyright   GPL-2.0
-!> \date        2022
-!> \file        unconfigure.f
-!> \note        See `LICENSE' for full license.
-!>              See `README.md' for project details.
-!>
-!> This alias is used in order to unset a configuration entry from this user's
-!> Git settings.
+!> \file unconfigure.f
 !!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!
+!> \author      Kevin Matthes
+!> \copyright   GPL-2.0
+!> \date        2022
+!> \note        See `LICENSE' for full license.
+!>              See `README.md' for project details.
+!>
 !> \brief   Remove a configuration entry from this user's Git settings.
 !>
 !> This alias is used in order to unset a configuration entry from this user's
