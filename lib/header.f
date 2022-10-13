ab0d8d168a79263f1a28ba525a143df7572b4161
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2022-10-13 13:58:38 +0200

    Create header.f

---
+++ lib/header.f
@@ -0,0 +1,51 @@
+!!!!!!!!!!!!!!!!!!!!!!!! GNU General Public License 2.0 !!!!!!!!!!!!!!!!!!!!!!!!
+!!                                                                            !!
+!! Copyright (C) 2022 Kevin Matthes                                           !!
+!!                                                                            !!
+!! This program is free software; you can redistribute it and/or modify       !!
+!! it under the terms of the GNU General Public License as published by       !!
+!! the Free Software Foundation; either version 2 of the License, or          !!
+!! (at your option) any later version.                                        !!
+!!                                                                            !!
+!! This program is distributed in the hope that it will be useful,            !!
+!! but WITHOUT ANY WARRANTY; without even the implied warranty of             !!
+!! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              !!
+!! GNU General Public License for more details.                               !!
+!!                                                                            !!
+!! You should have received a copy of the GNU General Public License along    !!
+!! with this program; if not, write to the Free Software Foundation, Inc.,    !!
+!! 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.                !!
+!!                                                                            !!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!
+!> \file header.f
+!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!
+!> \author      Kevin Matthes
+!> \copyright   GPL-2.0
+!> \date        2022
+!> \note        See `LICENSE' for full license.
+!>              See `README.md' for project details.
+!>
+!> \brief   The header information to be printed before the alias descriptions.
+!>
+!> This is a utility subroutine in order to call `self_information` and 
+!> `table_header` subsequently.  This is useful in order to encapsulate the call
+!> to both subroutines for the C bindings of this library.
+!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+      subroutine header
+      implicit none
+
+      call self_information
+      call table_header
+
+      end subroutine header
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
