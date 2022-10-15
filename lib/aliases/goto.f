e39165a5c4af35f7108d2c2c6658c1189ae02f4d
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2022-10-15 16:28:37 +0200

    Update goto.f

--- lib/aliases/goto.f
+++ lib/aliases/goto.f
@@ -40,7 +40,7 @@
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
       subroutine goto_git
-      use libgaf18, only: git
+      use, non_intrinsic :: libgaf18, only: git
       implicit none
 
       call git
