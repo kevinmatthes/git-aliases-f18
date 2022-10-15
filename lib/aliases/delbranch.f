418e2b26e495f4d72ac0f44857afb3371c72139a
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2022-10-15 16:27:48 +0200

    Update delbranch.f

--- lib/aliases/delbranch.f
+++ lib/aliases/delbranch.f
@@ -42,7 +42,7 @@
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
       subroutine delbranch
-      use libgaf18, only: git
+      use, non_intrinsic :: libgaf18, only: git
       implicit none
 
       call git
