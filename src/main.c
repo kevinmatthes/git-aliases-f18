7ed2eea71f77966a8428e2def24431d37ad7f4e8
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2022-10-18 19:08:08 +0200

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,50 @@
+/*!!!!!!!!!!!!!!!!!!!!!! GNU General Public License 2.0 !!!!!!!!!!!!!!!!!!!!!!*\
+|                                                                              |
+|  Copyright (C) 2022 Kevin Matthes                                            |
+|                                                                              |
+|  This program is free software; you can redistribute it and/or modify        |
+|  it under the terms of the GNU General Public License as published by        |
+|  the Free Software Foundation; either version 2 of the License, or           |
+|  (at your option) any later version.                                         |
+|                                                                              |
+|  This program is distributed in the hope that it will be useful,             |
+|  but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+|  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+|  GNU General Public License for more details.                                |
+|                                                                              |
+|  You should have received a copy of the GNU General Public License along     |
+|  with this program; if not, write to the Free Software Foundation, Inc.,     |
+|  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.                 |
+|                                                                              |
+\*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+
+/**
+ * \file main.c
+ */
+
+/******************************************************************************/
+
+/**
+ * \author      Kevin Matthes
+ * \copyright   GPL-2.0
+ * \date        2022
+ * \note        See `LICENSE' for full license.
+ *              See `README.md' for project details.
+ *
+ * \brief   The Git alias configuration program.
+ * \return  Exit code zero.
+ *
+ * This is the C99 edition of `git-aliases`.  Its behaviour is similar to that
+ * of the Fortran 2018 edition.
+ */
+
+#include "../include/libgaf18.h"
+
+int main (void)
+{
+    header ();
+    configure ();
+    return 0x0;
+}
+
+/******************************************************************************/
